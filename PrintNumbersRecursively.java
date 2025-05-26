bba5c0767a48009ffcb36fae7bf194807a7cdef7
Ramit Das <46995635+Ramitdas@users.noreply.github.com>
2025-05-26 11:11:30 +0530

    Update PrintNumbersRecursively.java

--- PrintNumbersRecursively.java
+++ PrintNumbersRecursively.java
@@ -1,6 +1,7 @@
 public class PrintNumbersRecursively {
     public static void main(String[] args) {
         increment(1); // Start from 1
+        decrement(1000); // Start from 1000
     }
 
     public static void increment(int k) {
@@ -8,6 +9,14 @@ public class PrintNumbersRecursively {
             return;
         }
         System.out.println(k);
-        increment(k + 1); // Call recursively
+        increment(k + 1); // Increase k in each recursive call
+    }
+
+     public static void decrement(int c) {
+        if (c < 1) { // Stop when c goes below 1
+            return;
+        }
+        System.out.println(c);
+        decrement(c - 1); // Decrease c in each recursive call
     }
 }
